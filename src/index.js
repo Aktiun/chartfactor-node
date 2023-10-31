5a0535789d5bfba456ed7ea1572576df55183b49
Edison Moya <e.j.moya.arias@gmail.com>
2023-10-31 07:55:41 -0500

    4 hr - Visibility of HTTP request/response for our Node data provider. How can we see what we are sending and the response? Best practices.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 'use strict';
 
+const { ClientRequestInterceptor } = require('@mswjs/interceptors/lib/interceptors/ClientRequest');
 require('make-promises-safe'); // installs an 'unhandledRejection' handler
 const path = require('path');
 const _ = require('lodash');
 let logFilePath = path.join(__dirname, '..', 'log', 'log.txt');
+require('dotenv').config();
 
 function buildFastify() {
     const fastify = require('fastify')({
         bodyLimit: 999999999,
         logger: {
-            level: 'info',
+            level: process.env.LOG_LEVEL || 'info',
             file: logFilePath
         }
     });
@@ -27,6 +29,33 @@ function buildFastify() {
         }
         fastify.log.info(`server listening on ${address}`);
     });
+
+    console.log('log level: ', process.env.LOG_LEVEL);
+
+    if (process.env.LOG_LEVEL === 'debug') {
+        const interceptor = new ClientRequestInterceptor()
+
+        interceptor.apply();
+
+        interceptor.on('response', (response, request) => {
+
+            const headers = {
+                'User-Agent': request.headers.get('User-Agent'),
+                'Content-Type': request.headers.get('Content-Type'),
+                'Accept': request.headers.get('Accept'),
+                'Accept-Encoding': request.headers.get('Accept-Encoding')
+            };
+
+            const reqJson = {
+                method: request.method,
+                url: request.url,
+                headers: headers,
+            };
+
+            fastify.log.debug(`***************************** external request ****************************`);
+            fastify.log.debug(reqJson);
+        })
+    }
 
     return fastify;
 }
